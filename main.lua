8a2d8f3ee3da5eb5233c5d4127cbb23f5e71362e
IWillyLovePython <104280094+IWillyLovePython@users.noreply.github.com>
2022-08-21 20:31:26 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,12 @@
 local gun = getsenv(game:GetService("Players").LocalPlayer.Backpack.Gun.HandgunScript)
 setupvalue(gun.UpdateAmmo, 2, 10000000000)
 
+--shreks in backroom fast gun
+func = hookfunction(wait, function(sec)
+    if sec == 0.36363636363636365 then return end
+    return func(sec)
+end)
+
 --shreks in back room one hit gun
 local metatable = getrawmetatable(game);
 local oldmetatable = metatable.__namecall
